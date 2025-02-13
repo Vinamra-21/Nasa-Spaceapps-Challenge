f1e810a9a374e0fbb7bfdaae4161cc687af40258
Vinamra Garg <gargvinamra21@gmail.com>
2025-02-13 19:27:14 +0530

    changes

--- spaceapps-kavtan/app/OurInsights/page.tsx
+++ spaceapps-kavtan/app/OurInsights/page.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
-import styles from './ourinsights.module.css'; 
+import styles from "./Ourinsights.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Link href="/CarbonEmission">
-        <button className={styles.button}>
-          Carbon Emission Model
-        </button>
+        <button className={styles.button}>Carbon Emission Model</button>
       </Link>
       <Link href="/MethaneEmission">
-        <button className={styles.button}>
-          Methane Emission Model
-        </button>
+        <button className={styles.button}>Methane Emission Model</button>
       </Link>
       <Link href="/Factors">
         <button className={styles.button}>
@@ -20,9 +16,7 @@ export default function Home() {
         </button>
       </Link>
       <Link href="/Effects">
-        <button className={styles.button}>
-          Effects of Climate Change
-        </button>
+        <button className={styles.button}>Effects of Climate Change</button>
       </Link>
     </div>
   );
